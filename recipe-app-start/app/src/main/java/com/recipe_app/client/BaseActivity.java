08a85b2d3e2515f7c8c0384c1281bf3f4296d9bc
Bernat Gómez <bernat.gomez@gmail.com>
2019-06-07 20:14:14 +0200

    Drawer

--- recipe-app-start/app/src/main/java/com/recipe_app/client/BaseActivity.java
+++ recipe-app-start/app/src/main/java/com/recipe_app/client/BaseActivity.java
@@ -1,10 +1,17 @@
 package com.recipe_app.client;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.NavigationView;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
 
 public class BaseActivity extends AppCompatActivity {
 
@@ -18,9 +25,50 @@ public class BaseActivity extends AppCompatActivity {
         this.setContentView(this.layout);
 
         this.setListeners();
+
+        this.configDrawer();
     }
 
     protected void setListeners() {
+
+    }
+
+    protected void configDrawer() {
+        DrawerLayout drawer = this.findViewById(R.id.drawer_view);
+        Toolbar toolbar = this.findViewById(R.id.toolbar_view);
+
+        if (toolbar != null) {
+            this.setSupportActionBar(toolbar);
+
+            this.getSupportActionBar().setDisplayShowHomeEnabled(true);
+            this.getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+            drawer.addDrawerListener(
+                new ActionBarDrawerToggle(this, drawer, toolbar, R.string.str_open, R.string.str_close)
+            );
+
+            NavigationView navView = this.findViewById(R.id.nav_view);
+
+            if (navView != null) {
+                navView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                        switch (menuItem.getItemId()) {
+                            case R.id.menu_rec:
+                                Navigator.toRecipes(BaseActivity.this, new Bundle());
+                                break;
+                            case R.id.menu_ingr:
+                                Navigator.toIngredients(BaseActivity.this, new Bundle());
+                                break;
+                            default:
+                                break;
+                        }
+
+                        return true;
+                    }
+                });
+            }
+        }
 
     }
 
